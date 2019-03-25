afba26398a5089aad4d1e5d55f2db3f7e7de253d
timbuk <ivoschwizer@gmail.com>
2019-03-25 08:41:50 +0100

    test

--- js/main.js
+++ js/main.js
@@ -9,10 +9,19 @@ $(document).ready(function () {
         $('.nav li').toggleClass('open');
 	});
     
+    //slider
+    
     $(function () {
     setInterval(function () {
         $('.overimg').fadeOut(1500).delay(5000).fadeIn(1500).delay(5000);
     }, 5000);
-});
+    });
+    
+    //show and hide
+    
+    $('.wl').click(function() {
+        $(this).siblings('.wl-hidden').fadeIn();
+        $(this).css("display", "none")
+    });
     
 });
